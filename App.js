3611e9e1269351c6b76f1df79ff4c862a4e2e06d
mcarneal <mcarneal09@yahoo.com>
2019-07-28 19:19:52 +0000

    added 404 path

--- App.js
+++ App.js
@@ -13,5 +13,9 @@ app.use(bodyParser.urlencoded({extended : false}))
 app.use(shopRoutes)
 app.use(adminRoutes)
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>')
+})
+
 
 app.listen(3000)
