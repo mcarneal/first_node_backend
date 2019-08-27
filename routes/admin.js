acec54241e2f2bc5ab8650c53e76976d906fde2a
mcarneal <mcarneal09@yahoo.com>
2019-08-27 18:21:27 +0000

    added ejs and cleaned up files

--- routes/admin.js
+++ routes/admin.js
@@ -9,13 +9,22 @@ const rootDir = require('../util/path')
 const products = []
 
 router.get('/add-product', (req, res, next) => {
-    res.render('add-product', { pageTitle : 'Add Product', path: '/admin/add-product', formsCSS: true, productCSS: true, activeAddProduct: true });
+    res.render('add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true
+    });
 })
 
 router.post('/add-product', (req, res, next) => {
-    products.push({ title : req.body.title })
+    products.push({
+        title: req.body.title
+    })
     res.redirect('/')
 })
 
 exports.routes = router
 exports.products = products
+
